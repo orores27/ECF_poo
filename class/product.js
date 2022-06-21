f1a1aeed676b936f57f8f10acfc95a43caadc130
orores27 <sabah.errifi@gmail.com>
2022-06-21 15:04:18 +0200

    ECF Fini

--- class/product.js
+++ class/product.js
@@ -1,7 +1,10 @@
 let prix = 0;
+let promo = 0;
+let price = 0;
 class Product {
     
-    constructor(name, price, promotion, discount){
+    constructor(produit, name, price, promotion, discount){
+        this.produit = produit;
         this.name = name;
         this.price = price;
         this.promotion = promotion;
@@ -10,15 +13,28 @@ class Product {
     reduction() {
     return this.promotion;
     }
-    afficherReduc() {
-     return 'Nom du produit:' + Product.name;
-    }
+    // afficherReduc() {
+    //  return 'Nom du produit:' + Product.name;
+    // }
     remise(){
         return this.discount;
     }
     nvPrix() {
         prix = this.price - this.discount;
-        return prix;
+        return prix.toFixed(2);
+        console.log(prix);
+    }
+     // ex 3
+    getCategorie() {
+        return this.name;
+    }
+    appliquerPromo(promo) {
+       price = this.price - promo;
+       return price;
+    }
+    appliquerPourcentage(pourcentage) {
+        price = this.price * pourcentage;
+       return price;
     }
 };
 
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import Product from "/class/product.js";
 import Categorie from "/class/categorie.js";
-    var Product1 = new Product('Spectacle Rodez', 43.00, 'non', 'non');
-    var Product2 = new Product('Spectacle Rodez', 43.00, 'non', 20*100/43);
-    var Product3 = new Product ('HP', 1999.99, 'oui', 20*100/1999.99);
-    var Product4 = new Product ('MAC', 1499.99, 'oui', 33*100/1499.99);
-    var Product5 = new Product ('ASUS', 3499.99, 'non', "non");
+    var Product1 = new Product('Michaël Gregorio', 'Spectacle Rodez', 43.00, 'non', 'non');
+    var Product2 = new Product('Daniel Guichard', 'Spectacle Rodez', 43.00, 'non', 43*20/100);
+    var Product3 = new Product ('Pc Portable Gaming MSI GL75 Leopard 10SFK-457FR 17.3/ Intel Core i7 16 Go RAM 256 Go SSD + 1 To SATA Noir', 'Ordinateurs Portables', 1999.99, 'oui', 20*1999.99/100);
+    var Product4 = new Product ('PC Portable Gaming Asus TUF505DV-HN232T 15.6/ AMD Ryzen 7 16 Go RAM 512 Go SSD Noir', 'Ordinateurs Portables', 1499.99, 'oui', 33*1499.99/100);
+    var Product5 = new Product ('PC Portable Gaming Acer Predator Triton 700 PT715-51-76D4 15.6/ Gaming Intel Core i7 32 Go RAM 256 Go SSD + 256 Go SATA Noir', 'Ordinateurs Portables', 3499.99, 'non', "non");
 
 //console.log(Product1.reduction());
 let i;
 let ProductArray = [Product1, Product2, Product3, Product4, Product5];
-// console.log(ProductArray);
+// ex 3 : méthode (getCatégorie) qui crée le lien entre la catégorie et le produit
+ProductArray.forEach(product => console.log(product.getCategorie()));
+
 
 
 /**
@@ -22,13 +24,47 @@ for (i = 0; i< ProductArray.length; i++) {
     if (ProductArray[i].reduction()=== 'oui') 
         console.log(ProductArray[i].name)
     };
-     
+ 
+    /** ex 5 */
+
 for (i = 0; i< ProductArray.length; i++) {
     
     if (ProductArray[i].remise() !== 'nom') 
-            console.log(ProductArray[i].name.price.prix);
-            nvPrix();
+            console.log(ProductArray[i].name, ProductArray[i].price, ProductArray[i].nvPrix());     
     };
+
+    /** ex 6 */
+    let ProductArray1 = [Product1.name, Product2.name, Product3.name, Product4.name, Product5.name];
+
+    ProductArray1.sort();
+    console.log(ProductArray1);
+
+    /**ex 7 */
+    let array = [];
+    for (i=0; i<ProductArray.length; i++) {
+        let produit = [ProductArray[i].name, ProductArray[i].produit]
+        array.push(produit);
+    }
+    console.log(array.sort());
+
+    /** ex 8
+     * 1/ Mettre un produit en promo : 
+     * a) créer une méthode appliquerPromo qui prend le produit et lui calcule une promo
+     * 2/ Affecter une remise
+     */
+
+     for (i = 0; i< ProductArray.length; i++) {
+        console.log(ProductArray[i].appliquerPromo(20));
+        console.log(ProductArray[i].appliquerPourcentage(50/100));
+
+        
+        // ProductArray.forEach(product => console.log(product.AppliquerPromo()));
+
+     }
+
+
+
+
 
 
 // ProductArray[i].discount;
